feb29b2cf18c2af042dd0bb5ee6fda5b0c5bc78a
sf-fresher-batch-2020 <72520930+sf-fresher-batch-2020@users.noreply.github.com>
2020-11-21 11:28:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-{
-  "name": "ticketapp-api",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "start": "npm install && node app.js"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "cors": "^2.8.5",
-    "express": "^5.0.0-alpha.8",
-    "joi": "^17.3.0",
-    "jsonwebtoken": "^8.5.1",
-    "mysql2": "^2.2.5"
-  },
-  "devDependencies": {
-    "nodemon": "^2.0.6"
-  },
-  "engines": {
-    "node": "15.x"
-  }
-}
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const cors = require('cors');
+const TOKEN_SECRET = "7bc78545b1a3923cc1e1e19523fd5c3f20b409509";
+const app = express()
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000
+
+
+const mysql = require("mysql2/promise");
+/*
+const pool = mysql.createPool({
+    host: process.env.DB_URL || "localhost",
+    port: 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "ticketapp_db",
+    connectionLimit: 1
+});*/
+
+app.get("/", (req, res) => res.send({ message: "REST API Service is working" }));
+
+app.listen(port, () => console.log(`Example app listening on port!`, port));
