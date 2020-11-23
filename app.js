37015ecd175438108b4d9442067a89fb3641226a
tushantkumar <tushantgarg75@gmail.com>
2020-11-23 11:46:59 +0530

    added files

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 
 
 const mysql = require("mysql2/promise");
-/*
+
 const pool = mysql.createPool({
     host: process.env.DB_URL || "localhost",
     port: 3306,
@@ -17,7 +17,47 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || "root",
     database: process.env.DB_NAME || "ticketapp_db",
     connectionLimit: 1
-});*/
+});
+
+// Routes
+//users
+app.post('/api/users', createUser);
+app.get('/api/users', getAllUsers);
+app.post('/api/users/login', login);
+
+// Functions
+async function createUser(req, res) {
+    let user = req.body;
+    let params = [user.name, user.email, user.password,user.role];
+    const result = await pool.query("INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)", params);
+    let id = result[0].insertId; 
+    res.status(200).json({id: id});
+}
+
+async function getAllUsers(req, res) {
+    const result = await pool.query("SELECT * FROM users");
+    res.status(200).json(result[0]);
+}
+
+async function login(req, res) {
+    const user = req.body;
+    let params = [user.email,user.password];
+    console.log(params);
+    const result = await pool.query("SELECT id, name, email, password FROM users WHERE email = ? and password= ?", params);
+    const users= result[0];
+    if (users.length ==0){
+        throw new Error("Invalid Login Credentials");
+      }
+    res.status(201).json(users[0]);
+}
+
+ // Create Commmon Error Handler
+ app.use(function (err, req, res, next) {
+    console.log("common error handler")
+    console.error(err);
+    res.json({errorMessage:err.message});
+})
+
 
 app.get("/", (req, res) => res.send({ message: "REST API Service is working" }));
 
